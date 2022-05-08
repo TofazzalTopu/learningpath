e8e747723aa40d297ebd0536711e2ee4a45cea08
taffozal <taffozal@basico.es>
2022-05-09 00:06:26 +0600

    add cors in security

--- src/main/java/com/gr/learningpath/config/secuirty/WebSecurityConfig.java
+++ src/main/java/com/gr/learningpath/config/secuirty/WebSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.gr.learningpath.config.secuirty;
 
 import com.gr.learningpath.constant.AppConstants;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -16,22 +19,24 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+import org.springframework.web.filter.CorsFilter;
+import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 
-
+@Log4j2
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@Import(SecurityProblemSupport.class)
 @ComponentScan(basePackages = "com.gr.learningpath")
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-	@Autowired
-	private UserDetailsService jwtUserDetailsService;
-
-	@Autowired
-	private JwtRequestFilter jwtRequestFilter;
+	private final CorsFilter corsFilter;
+	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	private final UserDetailsService jwtUserDetailsService;
+	private final JwtRequestFilter jwtRequestFilter;
+	private final SecurityProblemSupport problemSupport;
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -55,7 +60,38 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
-		httpSecurity.cors();
+//		httpSecurity.cors();
+		httpSecurity
+				.csrf()
+				.disable()
+				.addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+				.exceptionHandling()
+				.authenticationEntryPoint(problemSupport)
+				.accessDeniedHandler(problemSupport)
+				.and()
+				.headers()
+				.contentSecurityPolicy("default-src 'self'; frame-src 'self' data: blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data:; font-src 'self' https://fonts.gstatic.com data:;object-src 'self' blob:")
+				.and()
+				.referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
+				.and()
+				.featurePolicy("geolocation 'none'; midi 'none'; sync-xhr 'none'; microphone 'none'; camera 'none'; magnetometer 'none'; gyroscope 'none'; fullscreen 'self'; payment 'none'")
+				.and()
+				.frameOptions()
+				.deny()
+				.and()
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.authorizeRequests()
+				.antMatchers(
+						"/api/login",
+						"/api/register"
+				).permitAll()
+				.antMatchers("/api/**").authenticated()
+				.and()
+				.httpBasic();
+//				.and()
+//				.apply(securityConfigurerAdapter());
 		httpSecurity.csrf().disable()
 				.authorizeRequests()
 				.antMatchers(AppConstants.AUTH_WHITELIST).permitAll()
@@ -78,6 +114,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		// Add a filter to validate the tokens with every request
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+//		httpSecurity.csrf().disable()
+//				.authorizeRequests()
+//				.antMatchers(AppConstants.AUTH_WHITELIST).permitAll()
+//				.anyRequest()
+//				.permitAll()
+//				.and()
+//				.formLogin()
+//				.permitAll()
+//				.and()
+//				.logout()
+//				.invalidateHttpSession(true)
+//				.deleteCookies()
+//				.and()
+//				// make sure we use stateless session; session won't be used to
+//				// store user's state.
+//				.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+//				.and()
+//				.sessionManagement()
+//				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//
+//		// Add a filter to validate the tokens with every request
+//		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
 	}
 
 }
